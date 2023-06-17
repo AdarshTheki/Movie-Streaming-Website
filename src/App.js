cda29a09b88a498b0633deada8c6a7706b59bb7d
Adarsh Verma <adarshverma549@gmail.com>
2023-06-17 14:00:46 +0530

    main comp. - index.js & App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import requests from "./Server/Requests.js";
+import Nav from "./Components/Nav.js";
+import Banner from "./Components/Banner.js";
+import Row from "./Components/Row.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* NavBar */}
+      <Nav/>
+      {/* Banner */}
+      <Banner />
+      {/* Rows of the Shows*/}
+      <Row
+        title='Netflix Originals'
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row
+        title='Trending Now'
+        fetchUrl={requests.fetchTrending}
+        isLargeRow={true}
+      />
+      <Row
+        title='Top Rated'
+        fetchUrl={requests.fetchTopRated}
+        isLargeRow={true}
+      />
+      <Row
+        title='Action Movies'
+        fetchUrl={requests.fetchActionMovies}
+        isLargeRow={true}
+      />
+      <Row
+        title='Comedy Movies'
+        fetchUrl={requests.fetchComedyMovies}
+        isLargeRow={true}
+      />
+      <Row
+        title='Horror Movies'
+        fetchUrl={requests.fetchHorrorMovies}
+        isLargeRow={true}
+      />
+      <Row
+        title='Romance Movies'
+        fetchUrl={requests.fetchRomanceMovies}
+        isLargeRow={true}
+      />
+      <Row
+        title='Documentaries'
+        fetchUrl={requests.fetchDocumentaries}
+        isLargeRow={true}
+      />
     </div>
   );
 }
-
 export default App;
